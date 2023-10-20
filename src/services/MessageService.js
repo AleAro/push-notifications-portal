ae19a8e30a1b25bd82142fdb5776bcc359d9fbdc
German Andres <german@Germans-MacBook-Air.local>
2023-10-20 10:47:12 -0500

    Added aurthentication

--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -1,28 +1,40 @@
 import axios from 'axios';
 
 const Message_API_BASE_URL = "https://onyni7ymnl.execute-api.us-east-2.amazonaws.com/nDev/pushNotifications";
-
+const headers = {
+    Authorization: "Hola Mundo",
+  };
 class MessageService {
-
+   
     getMessages(){
         console.log("getting messages")
-        return axios.get(Message_API_BASE_URL);
+        return axios.get(Message_API_BASE_URL, {
+            headers: headers,
+          });
     }
 
     createMessage(Message){
-        return axios.post(Message_API_BASE_URL, Message);
+        return axios.post(Message_API_BASE_URL, Message, {
+            headers: headers,
+          });
     }
 
     getMessageById(MessageId){
-        return axios.get(Message_API_BASE_URL + '/' + MessageId);
+        return axios.get(Message_API_BASE_URL + '/' + MessageId, {
+            headers: headers,
+          });
     }
 
     updateMessage(Message, MessageId){
-        return axios.put(Message_API_BASE_URL + '/' + MessageId, Message);
+        return axios.put(Message_API_BASE_URL + '/' + MessageId, Message, {
+            headers: headers,
+          });
     }
 
     deleteMessage(MessageId){
-        return axios.delete(Message_API_BASE_URL + '/' + MessageId);
+        return axios.delete(Message_API_BASE_URL + '/' + MessageId, {
+            headers: headers,
+          });
     }
 }
 
